bb92eea102cc1336da080024db4122d37948d54f
vimalv1996 <155906070+vimalv1996@users.noreply.github.com>
2024-03-04 18:46:05 -0500

    Create project.py

---
+++ project.py
@@ -0,0 +1,16 @@
+import os
+
+list1 = [18, 3, 3, 65, 1, 191]
+list1.reverse()
+print("The reversed list is", list1)
+
+list2 = [91, 6, 120, 34, 6, 54, 188, 81, 35, 28, 3430]
+list2.sort()
+print("The sorted list is", list2)
+
+list3 = []
+list3 = list1.copy()
+print("List3 -", list3)
+
+index_value = list2[2:6]
+print("The index values are", index_value)
